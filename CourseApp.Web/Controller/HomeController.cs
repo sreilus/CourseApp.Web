3781de8a320206c6d2cf8d7f11d714e633d0557a
Muhammet Akvardar <34101771+sreilus@users.noreply.github.com>
2020-03-03 14:14:51 +0300

    Initial Commit

---
+++ CourseApp.Web/Controller/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CourseApp.Web.Models;
+
+namespace CourseApp.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+			var course = new Course();
+			course.Id = 1;
+			course.Name = "Asp.Net Core 2 Kursu";
+			return View(course);
+        }
+    }
+}
